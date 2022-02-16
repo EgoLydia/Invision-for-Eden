f0fa68e1ca9bc6b836927b90589e73d493ff6ce9
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-02-16 19:25:50 +0100

    Feature: Clone view meal card

--- script.js
+++ script.js
@@ -2,11 +2,64 @@ const viewMeal = document.getElementById("viewMeal");
 const modal = document.getElementById("modal");
 const closeBtn = document.getElementById("close");
 
-viewMeal.addEventListener('click', openModal);
+viewMeal.addEventListener('click', onViewMealClick);
 closeBtn.addEventListener ('click', closeModal);
 
-function openModal(){
-    modal.style.display="flex";
+function toggleExpansion(element, to, duration) {
+    return new Promise(res => {
+        requestAnimationFrame(() => {
+            element.style.transition = `
+            width ${duration} ease-in-out,
+            height ${duration} ease-in-out,
+            left ${duration} ease-in-out,
+            top ${duration} ease-in-out
+            `;
+    
+            element.style.top = to.top;
+            element.style.left = to.left;
+            element.style.width = to.width;
+            element.style.height = to.height;
+        });
+        
+         setTimeout(res, duration)
+    });
+};
+
+async function fadeContent(element, duration = 300){
+    return new Promise(res => {
+        [...element.children].forEach((child) => {
+            requestAnimationFrame(() => {
+                child.style.transition = `opacity ${duration}ms linear`;
+                child.style.opacity = '0';
+            });
+        })
+        setTimeout(res, duration);
+    })
+}
+
+async function onViewMealClick(e) {
+
+    const viewMealCard = e.currentTarget;
+    const viewMealCardClone = viewMealCard.cloneNode(true);
+    const rect = viewMealCard.getBoundingClientRect();
+    const top = rect.top;
+    const left = rect.left;
+    const width = rect.width;
+    const height = rect.height;
+
+    viewMealCardClone.style.position = 'absolute';
+    viewMealCardClone.style.top = top + 'px';
+    viewMealCardClone.style.left = 28 + 'px';
+    viewMealCardClone.style.width = width + 'px';
+    viewMealCardClone.style.height = height+ 'px';
+
+    viewMealCard.style.opacity = '0';
+    viewMealCard.parentNode.appendChild(viewMealCardClone);
+
+    //fadeContent(viewMealCardClone);
+    toggleExpansion(viewMealCardClone, {top: `${top - 180}px`, left: `${0}px`, width: `${width + 56}px`, height:`${height}px`}, '0.7s');
+
+    // modal.style.display="flex";
 }
 
 function closeModal(){
